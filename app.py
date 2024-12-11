a87203e72e3e5107c78357e7ea74f48b50c73ef7
alternock <montilla269@gmail.com>
2024-12-12 00:24:21 +0100

    fc

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/things")
+def things():
+    return jsonify({
+        "payload":["foo","bar","baz","charlie","echo","hotel"]
+    })
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
